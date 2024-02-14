92ec400c9dd138b762289982e570abbeb88b1129
Madhanprasath-D <madhanprasath.dev@gmail.com>
2024-02-14 23:25:37 +0530

    data_to_json

--- server.js
+++ server.js
@@ -1,68 +1,56 @@
-const { json } = require('express')
-const fs=require('fs')
+const fs = require('fs')
 const path = require('path')
+const fspromise = require('fs').promises
 
-const express=require('express')
-
-const app =express()
-const Shopify = require('shopify-api-node')
-
-const shopify = new Shopify({
-    shopName:'onstoreproject.myshopify.com',
-    accessToken:'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'
-})
-
-const data = async () =>{
+function loadjson_data(file_name){
     try{
-        const jsonfile = await shopify.product.list({limit:5}).then((products) => console.log(products)).catch ((err)=> console.log(err))
-        console.log(jsonfile)
-    }catch (err){
-        console.log(err)
+        const data = fs.readFileSync(path.join(__dirname,'DataFiles',file_name),'utf-8')
+        return JSON.parse(data)
+    }
+    catch(err){
+        console.log("Error in loadjson",err)
     }
 }
 
-data()
+function savejson(file_name,dataline){
+    try{
+        fs.writeFileSync(path.join(__dirname,'DataFiles',file_name),JSON.stringify(dataline))
+        console.log("data wrired")
+    }
+    catch (err){
+        console.error(err);
+    }
+}
+const fileOps = ()=>{  
+    try{
+        const rawdata=loadjson_data('raw.json').orders
+        const database=loadjson_data('data.json')
+        for(i=0;i<rawdata.length;i++){
+            const specified_data={}
+            specified_data['Name']=rawdata.first_name+rawdata.last_name;
+            specified_data['Customer_id']=rawdata.customer.id;
+            specified_data['Email']=rawdata.customer.email;
+            specified_data['Order_count']=rawdata.customer.orders_count;
+            specified_data['Total_spend']=rawdata.customer.total_spend
+            specified_data['Order_detials']={}
+            specified_data.Order_detials['Order_id']=rawdata.id;
+            specified_data.Order_detials['Created_at']=rawdata.created_at;
+            specified_data.Order_detials['Total_price']=rawdata.current_total_price;
+            specified_data.Order_detials['Order_Number']=rawdata.order_Number;
+            specified_data.Order_detials['Discount']=rawdata.discount;
 
-// Api new access key - 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
+            database.push(specified_data);
+        }
+        savejson('data.json', database);
+    }
+    catch (err){
+        console.error("Error in fileOps",err);
+    }
+}
 
-// // Api inte link sytx : https://apikey:apitoken@storename/admin/api/2022-10/orders.json
-
-// fs.readFile(path.join(__dirname,'DataFiles','data.json'),'utf-8',(err, data) =>{
-//     if (err) throw err;
-//     try{
-//         const JsonData = JSON.parse(data);
-//         console.log(JsonData.name);
-//     }
-//     catch (err){
-//         console.error("Error in parse",err)
-//     }
-
-// })
-
-
-// const newData ={
-//     "name":"Karthick",
-//     "orders":20,
-//     "address":"Karpagam Academy of Higher Education",
-// }
-
-// fs.appendFile(path.join(__dirname,'DataFiles','data.json'),JSON.stringify(newData ,null,2), err=>{
-//     if (err) throw err;
-//     console.log("file Writed...")
-// })
-
-// fs.readFile(path.join(__dirname,'DataFiles','data.json'),'utf-8',(err,data)=>{
-//     if (err) throw err
-//     const jsondata=JSON.parse(data)
-//     console.log(jsondata.order)
-//     jsondata.order+=1
-//     fs.writeFile(path.join(__dirname,'DataFiles','data.json'),JSON.stringify(jsondata, null ,4),err=>{
-//         if(err) throw err;
-//         console.log("data writed...")
-//     })
-// })
+fileOps()
 
 process.on('uncaughtException',err=>{
-    console.error("Erroer",err);
-    process.exit(1);
+    console.log("Error",err);
+    process.exit(0);
 })
